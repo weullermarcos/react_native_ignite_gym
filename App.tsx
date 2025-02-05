585d20aa63618738de7ee151253ee372ba2f9a79
Weuller <weullermarcos@hotmail.com>
2025-02-05 16:45:35 -0300

    Adicionando assets

--- App.tsx
+++ App.tsx
@@ -1,20 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { useFonts, Roboto_700Bold, Roboto_400Regular } from "@expo-google-fonts/roboto";
 
 export default function App() {
+
+  const [fonstLoaded] = useFonts({Roboto_700Bold, Roboto_400Regular});
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#202024' }}>
+
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
+
+      {fonstLoaded ? <Text>Home</Text> : <View />}
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
