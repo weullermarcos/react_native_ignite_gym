187b519ecdfcb3eef497c115d20ee2f9613eb098
Weuller <weullermarcos@hotmail.com>
2025-02-17 14:48:45 -0300

    Criando componente de grupos de exercicios

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,11 +1,33 @@
+import { useState } from 'react';
+import { FlatList } from 'react-native';
+import { Group } from '@components/Group';
 import { HomeHeader } from '@components/HomeHeader';
-import { Center, VStack } from '@gluestack-ui/themed';
+import { Center, HStack, VStack } from '@gluestack-ui/themed';
 
 
 export function Home(){
+
+    const [groups, setGroups] = useState(["Costas", "Bíceps", "Tríceps", "Ombro"]);
+    const [groupSelected, setGroupSelected] = useState("Costas");
+
     return (
         <VStack flex={1} bg="$gray700">
             <HomeHeader />
+
+            <FlatList 
+                data={groups} 
+                keyExtractor={item => item}
+                renderItem={({ item }) => (
+                    <Group name={item} isActive={groupSelected === item} onPress={ () => setGroupSelected(item) } />
+                )}
+                horizontal={true}
+                showsHorizontalScrollIndicator={false}
+                contentContainerStyle={{ paddingHorizontal: 32 }}
+                style={{ marginVertical: 40, maxHeight: 40, minHeight: 44 }}
+            >
+
+            </FlatList>
+            
         </VStack>
     )
 }
